2d91ea6b0f1f855b4efff25b29e85f08a2cc5a65
Krxwallo <59575572+krxwallo@users.noreply.github.com>
2023-07-22 15:02:28 +0200

    Update to godot-lib-4.2.dev1 - kotlin 1.9.0 - gradle 8.2.1 - android build tools 8.1.0-rc01 - java 17 - target/compile sdk: 33

--- app/src/main/java/com/timoschwarzer/godotengine/applinks/AppLinks.kt
+++ app/src/main/java/com/timoschwarzer/godotengine/applinks/AppLinks.kt
@@ -3,17 +3,19 @@ package com.timoschwarzer.godotengine.applinks
 import org.godotengine.godot.Godot
 import org.godotengine.godot.plugin.GodotPlugin
 
+@Suppress("unused")
 class AppLinks(godot: Godot?) : GodotPlugin(godot) {
     override fun getPluginName(): String {
         return "AppLinks"
     }
 
-    fun getUrl(): String? {
-        val url = Godot.getCurrentIntent()?.dataString
-        Godot.getCurrentIntent().data = null
+    fun getUrl(): String? = activity?.intent?.run {
+        val url = dataString
+        data = null
         return url
     }
 
+    @Suppress("OVERRIDE_DEPRECATION")
     override fun getPluginMethods(): MutableList<String> {
         return mutableListOf("getUrl")
     }
